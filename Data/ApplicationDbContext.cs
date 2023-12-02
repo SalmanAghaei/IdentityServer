cf6597d73d7035fe881cf1ce25cdc0ad041334da
SalmanAghaei <41117076+SalmanAghaei@users.noreply.github.com>
2023-12-02 12:57:17 +0330

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        Database.Migrate();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<ApplicationUser>().HasKey(x=>x.Id);
+        builder.Entity<IdentityRole>().HasKey(x=>x.Id);
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
